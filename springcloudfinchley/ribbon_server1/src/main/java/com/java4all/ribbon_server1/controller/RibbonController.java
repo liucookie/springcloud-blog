73f138bc2da9c2c339c8d95afee57667d35deb83
liuxuan <liuxuan>
2020-07-21 17:20:08 +0800

    commit project

---
+++ springcloudfinchley/ribbon_server1/src/main/java/com/java4all/ribbon_server1/controller/RibbonController.java
@@ -0,0 +1,31 @@
+package com.java4all.ribbon_server1.controller;
+
+import com.java4all.ribbon_server1.service.RibbonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description:
+ *
+ * @author wangzx
+ * @version v1.0
+ * @date 2018/11/23 16:00
+ */
+@RestController
+@RequestMapping(value = "ribbon")
+public class RibbonController {
+
+  @Autowired
+  private RibbonService ribbonService;
+
+  @GetMapping(value = "getCompany/{id}")
+  public String getCompany(@PathVariable("id") String id){
+    String company = ribbonService.getCompany(id);
+    System.out.println(company);
+    return company;
+  }
+}
